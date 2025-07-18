36f2fcf8470043c79f256c1e704fa50a37beedf6
norbyv1 <127006485+norbyv1@users.noreply.github.com>
2025-07-17 22:08:18 -0700

    Update Implementations.lua

--- Implementations.lua
+++ Implementations.lua
@@ -2,53 +2,57 @@ local _ENV = (getgenv or getfenv)()
 
 local Implementations = {}
 
--- from number to boolean
+local str_find = string.find
+local str_rep = string.rep
+local tostr = tostring
+local type = type
+
 function Implementations.toBoolean(n)
 	return n ~= 0
 end
 
--- an easy way to escape string, most developers better code like this!!!!
 function Implementations.toEscapedString(s)
-	if type(s) == "string" then
-		local hasQuotationMarks = string.find(s, '"') ~= nil
-		local hasApostrophes = string.find(s, "'") ~= nil
-
-		if hasQuotationMarks and hasApostrophes then
-			return "[[" ..s.. "]]"
-		elseif hasQuotationMarks and not hasApostrophes then
-			return "'" ..s.. "'"
-		end
-
-		return '"' ..s.. '"'
+	if type(s) ~= "string" then
+		return tostr(s)
 	end
 
-	return tostring(s)
+	local hasDouble = str_find(s, '"', 1, true)
+	local hasSingle = str_find(s, "'", 1, true)
+
+	if hasDouble and hasSingle then
+		return "[[" .. s .. "]]"
+	elseif hasDouble then
+		return "'" .. s .. "'"
+	else
+		return '"' .. s .. '"'
+	end
 end
 
--- picks indexing method based on characters in a string
 function Implementations.formatIndexString(s)
-	if type(s) == "string" then
-		local validDirectPattern = "^[%a_][%w_]*$"
-		if string.find(s, validDirectPattern) then
-			return `.{s}`
-		end
-		return `["{s}"]`
+	if type(s) == "string" and str_find(s, "^[%a_][%w_]*$") then
+		return "." .. s
 	end
-
-	return tostring(s)
+	return "[" .. Implementations.toEscapedString(s) .. "]"
 end
 
--- add left side character padding to x
 function Implementations.padLeft(x, char, padding)
-	return string.rep(char, padding - #tostring(x)) .. x
+	x = tostr(x)
+	local diff = padding - #x
+	if diff > 0 then
+		return str_rep(char, diff) .. x
+	end
+	return x
 end
 
--- add right side character padding to x
 function Implementations.padRight(x, char, padding)
-	return x .. string.rep(char, padding - #tostring(x))
+	x = tostr(x)
+	local diff = padding - #x
+	if diff > 0 then
+		return x .. str_rep(char, diff)
+	end
+	return x
 end
 
--- returns true if passed string is a key pointing to a Roblox global
 function Implementations.isGlobal(s)
 	return _ENV[s] ~= nil
 end
